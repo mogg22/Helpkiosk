a676b6a0484304c6a8241035865cc838adba2fd8
gardening-y <60008786@naver.com>
2023-08-01 17:12:21 +0900

    build: user모델 추가

--- helpkiosk/users/models.py
+++ helpkiosk/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  nickname = models.CharField(max_length=20)
+  number = PhoneNumberField(region='KR')
+  seller = models.BooleanField(default=False) # 판매자/구매자

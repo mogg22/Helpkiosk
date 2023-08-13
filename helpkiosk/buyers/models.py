f7286d1354eeb1b4f236ff0165319c8e4265b56e
76MINJI <mnxxj1128@naver.com>
2023-08-14 00:52:51 +0900

    Revert "feat: cart, payment 수정"
    This reverts commit ee8066c47ebddd0637c8df76d42b577f2776bc51.

--- helpkiosk/buyers/models.py
+++ helpkiosk/buyers/models.py
@@ -1,30 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
 from sellers.models import *
-# from phonenumber_field.modelfields import PhoneNumberField
+from phonenumber_field.modelfields import PhoneNumberField
+
+# class Cart(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+#     # options = models.ManyToManyField(MenuOption, blank=True)
+#     quantity = models.PositiveIntegerField(default=1)
+#     date_added = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return f'{self.user.username} - {self.menu.name}'
+
 
 class Cart(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    menu = models.ForeignKey(Menu, on_delete=models.CASCADE, null=True)
-    option = models.ForeignKey(Option, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     market = models.ForeignKey(Market, on_delete=models.CASCADE, null=True, blank=True)
-    quantity = models.PositiveIntegerField(default=1)
-    order = models.BooleanField(default=False) # False 포장, True 매장식사
-
-    def get_total_price(self):
-        return self.menu.price * self.quantity
-
-    def save(self, *args, **kwargs):
-        if self.menu:
-            self.market = self.menu.category.market
-            
-            # 선택된 옵션에 따라 매장식사 여부 결정
-            if self.option and self.option.is_dine_in_option():
-                self.order = True
-            else:
-                self.order = False
-                
-        super().save(*args, **kwargs)
+    order = models.BooleanField(default=False) # False 포장, True 배달
+    
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
@@ -39,8 +33,4 @@ class Payment(models.Model):
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateTimeField(auto_now_add=True)
     need = models.TextField(null=True, blank=True)
-    payment_method = models.CharField(max_length=100, default="픽업시 결제")
-    phone_number = models.CharField(max_length=20, default="010-1234-5678")
-
-    def __str__(self):
-        return f"Payment for Cart {self.cart.id}"
+    # 결제 수단 필드
